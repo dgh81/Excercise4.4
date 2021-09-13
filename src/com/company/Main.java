cd208bb64c87fadc980b3340ff0fb7d5718dd7ee
Daniel <danielguldberg@gmail.com>
2021-09-13 14:23:42 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Se powerpoint
+        int myNumber; // En variabel skifter til grå tekst når den ikke anvendes.
+        System.out.println("boo!") + 7; //Rød fejlkode fortæller at man ikke kan udføre + på 'void'.
+    }
+}
